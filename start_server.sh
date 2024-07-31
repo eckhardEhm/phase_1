f2d2a902b7a1bcde9ddb72cbe859fc4335a24ff3
Eckhard <eckhard@macbook-air.lan>
2024-07-31 09:34:42 +0700

    refactored to run webrick

--- start_server.sh
+++ start_server.sh
@@ -3,8 +3,5 @@
 # Define server port
 PORT=8080
 
-# Start the Ruby HTTP server
-echo "Starting HTTP server with Ruby on http://localhost:$PORT..."
-
 open http://localhost:$PORT
-ruby -run -e httpd . -p $PORT
+ruby server.rb
